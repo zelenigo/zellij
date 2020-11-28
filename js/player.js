a5a6e038c5c7a92a6f2f1f21350fae14838984c3
Victor Nastasa <victornast@gmail.com>
2020-11-28 13:15:36 +0100

    add pick track and move tiles

--- js/player.js
+++ js/player.js
@@ -3,6 +3,7 @@ class Track {
     this.maxSlots = size;
     this.usedSlots = 0;
     this.tileID = 0;
+    this.available = false;
   }
 }
 
@@ -43,7 +44,67 @@ class Player {
     ];
   }
 
+  drawPenaltyLine() {
+    for (let i = 0; i < this.penaltyLine.length; i++) {
+      ctx.drawImage(
+        tileImage,
+        this.penaltyLine[i] * 50,
+        0,
+        50,
+        50,
+        4 + (50 + 8) * i,
+        (8 + 50) * 5 + 12,
+        50,
+        50
+      );
+    }
+  }
+
+  drawWorkers() {
+    for (let track = 0; track < 5; track++) {
+      for (let tile = 0; tile < this.storage[track].usedSlots; tile++) {
+        ctx.drawImage(
+          tileImage,
+          this.storage[track].tileID * 50,
+          0,
+          50,
+          50,
+          290 + 4 + tile * (50 + 8),
+          4 + track * (50 + 8),
+          50,
+          50
+        );
+      }
+    }
+  }
+
   draw() {
     ctx.drawImage(playerBoardImage, 0, 0);
+    this.drawPenaltyLine();
+    this.drawWorkers();
+  }
+
+  drawAvailableTrack(color) {
+    this.checkAvailableTrack(color);
+    for (let i = 0; i < 5; i++) {
+      if (this.storage[i].available) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(290, i * (50 + 8), (i + 1) * (50 + 8), 50 + 8);
+      }
+    }
+  }
+
+  checkAvailableTrack(color) {
+    for (let i = 0; i < 5; i++) {
+      if (
+        this.storage[i].usedSlots < this.storage[i].maxSlots &&
+        (this.storage[i].tileID === 0 || this.storage[i].tileID === color) &&
+        this.wallFill[i][this.wallIDs[i].indexOf(color)] === 0
+      ) {
+        this.storage[i].available = true;
+      } else {
+        this.storage[i].available = false;
+      }
+    }
   }
 }
